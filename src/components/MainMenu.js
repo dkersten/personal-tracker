6527af98cf8d8856292b4b40f49fbf42ff8f5302
dkersten <dkersten1994@gmail.com>
2021-01-20 21:19:06 -0500

    started styling menu

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -9,16 +9,23 @@ const MainMenu = () => {
 
         const NavItem = styled.li`
             color: #f7f7f7;
-            font-size: 1.2rem;
+            font-size: 1.5rem;
+            font-family: 'Source Sans Pro', sans-serif;
+            font-weight: 600;
+            margin-bottom: 1rem;
+        `
+        const IconContainer = styled.span`
+            display: inline-block;
+            margin-right: .75rem;
         `
     
     return (
-            <MenuNav>
-                <NavItem>Add Activities</NavItem>
-                <NavItem>Tracker</NavItem>
-                <NavItem>Goals</NavItem>
-                <NavItem>Stats</NavItem>
-            </MenuNav>
+        <MenuNav>
+            <NavItem><IconContainer><i class="fal fa-plus"></i></IconContainer> Add Activities</NavItem>
+            <NavItem><IconContainer><i class="fal fa-calendar-alt"></i></IconContainer> Tracker</NavItem>
+            <NavItem><IconContainer><i class="fal fa-trophy"></i></IconContainer> Goals</NavItem>
+            <NavItem><IconContainer><i class="fal fa-analytics"></i></IconContainer> Stats</NavItem>
+        </MenuNav>
     )
 }
 
