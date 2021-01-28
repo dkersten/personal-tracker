b90d543793025ccc60cd6915064c26093356326b
dkersten <dkersten1994@gmail.com>
2021-01-27 20:52:56 -0500

    continued work on controlled add activity form

--- src/components/AddActivityForm.js
+++ src/components/AddActivityForm.js
@@ -4,13 +4,21 @@ const AddActivityForm = () => {
 
     const [ activityName, setActivityName ] = useState("")
     const [ activityDescription, setActivityDescription ] = useState("")
+    const [ activityCategory, setActivityCategory ] = useState("misc")
 
     const handleActivityNameChange = e => {
         setActivityName(e.target.value)
+        console.log(activityName)
     }
 
     const handleActivityDescriptionChange = e => {
         setActivityDescription(e.target.value)
+        console.log(activityDescription)
+    }
+
+    const handleActivityCateogryChange = e => {
+        setActivityCategory(e.target.value)
+        console.log(activityCategory)
     }
 
     const handleSubmit = e => {
@@ -23,6 +31,16 @@ const AddActivityForm = () => {
             <form onSubmit={handleSubmit}>
 
                 <input name="activityName" onChange={handleActivityNameChange} value={activityName} placeholder="Activity Name"/>
+
+                <label>
+                    Category:
+
+                    <select name="category" onChange={handleActivityCateogryChange}>
+                        <option value="misc"></option>
+                        <option value="career">Career</option>
+                        <option value="personal-wellness">Personal Wellness</option>
+                    </select>
+                </label>
 
                 <input name="activityDescription" onChange={handleActivityDescriptionChange} value={activityDescription} placeholder="Activity Description"/>
 
