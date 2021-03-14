d627c41c75377baa013abf77e0b7ed67852c4500
dkersten <dkersten1994@gmail.com>
2021-03-13 20:42:56 -0500

    fully implemented date component aon add activity form

--- src/components/AddActivityForm.js
+++ src/components/AddActivityForm.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import 'carbon-components/css/carbon-components.min.css';
-  import { DatePicker, DatePickerInput } from 'carbon-components-react';
-
 import { ButtonStandard, defaultFont } from '../mixins'
+
+import TableDatePicker from './DatePicker'
 
 // styling
     const FormContainer = styled.div`
+
+    .testing {
+        color: #f7f7f7;
+    }
         
         form {
             margin-bottom: 3rem;
@@ -22,7 +25,7 @@ import { ButtonStandard, defaultFont } from '../mixins'
                 flex-basis: calc(50% - 1.5rem);
             }
             
-            button {
+            button:not(.react-datepicker__navigation) {
                 margin-left: right;
                 align-self: flex-start;
                 ${ defaultFont };
@@ -53,13 +56,39 @@ import { ButtonStandard, defaultFont } from '../mixins'
                 }
             }
         }
+        
+        /* overwrite date picker library styling */
+        .react-datepicker-wrapper {
+            width: 100%;
+        }
+
+        .react-datepicker__month-container {
+            background: #303538;
+
+            * {
+                color: #f7f7f7;
+            }
+        }
+
+        .react-datepicker__header {
+            background: #252A2E;
+        }
+
+        .react-datepicker__triangle {
+            border-bottom-color: #252A2E !important;
+        }
+
+        .react-datepicker__day--keyboard-selected {
+            background: #03A5F5;
+        }
     `
 
 const AddActivityForm = () => {
 
     const [ activityName, setActivityName ] = useState("")
     const [ activityDescription, setActivityDescription ] = useState("")
-    const [ activityDate, setActivityDate] = useState(new Date())
+    const [ activityDate, setActivityDate] = useState(null)
+
     const [ activityCategory, setActivityCategory ] = useState("misc")
 
     const handleActivityNameChange = e => {
@@ -70,7 +99,12 @@ const AddActivityForm = () => {
         setActivityDescription(e.target.value)
     }
 
-    const handleActivityDateChange = date => {
+    // const handleActivityDateChange = e => {
+    //     console.log(e.target.value)
+    //     setActivityDate([date])
+    // }
+
+    const handleActivityDateChange = (date) => {
         setActivityDate(date)
     }
 
@@ -80,6 +114,7 @@ const AddActivityForm = () => {
 
     const handleSubmit = e => {
         e.preventDefault()
+        console.log(activityName, activityCategory,  activityDescription)
     }
 
     return (
@@ -101,8 +136,10 @@ const AddActivityForm = () => {
                         Activity Category:
                         <select key={3} value={activityCategory} id="activityCategory" name="category" onChange={handleActivityCateogryChange}>
                             <option value="misc"></option>
-                            <option value="career">Career</option>
-                            <option value="personal-wellness">Personal Wellness</option>
+                            <option value="Career Growth">Career Growth</option>
+                            <option value="Exercise">Exercise</option>
+                            <option value="Personal Finance">Personal Finance</option>
+                            <option value="Personal Growth">Personal Growth</option>
                         </select>
                     </label>
                 </div>
@@ -110,24 +147,8 @@ const AddActivityForm = () => {
                 <div className="input-container">
                     <label>
                         Activity Date:
-                        <DatePicker dateFormat="m/d/Y" datePickerType="single">
-                            <DatePickerInput
-                                id="date-picker-single"
-                                placeholder="mm/dd/yyyy"
-                                type="text"
-                                key={4}
-                                selected={ activityDate }
-                                onChange={handleActivityDateChange}
-                                name="date"
-                            />
-                        </DatePicker>
-    
-                        <DatePicker
-                            key={4}
-                            selected={ activityDate }
-                            onChange={handleActivityDateChange}
-                            name="date"
-                            dateFormat="MM/dd/yyyy"
+                        <TableDatePicker 
+                            updateDate={handleActivityDateChange}
                         />
                     </label>
                 </div>
@@ -145,10 +166,12 @@ const AddActivityForm = () => {
 
             </form>
 
-            <div>
+            <div className="testing">
                 Name: {activityName}
                 <br />
                 Category: {activityCategory}
+                <br />
+                Activity Date: {activityDate ? activityDate.toString() : "Not selected"}
                 <br />
                 Activity Description: {activityDescription}
             </div>
--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,36 +1,26 @@
-import React from 'react';
-import { DatePicker, datePickerInput} from 'react-datepicker'
+import React, { useState } from "react";
 
-const props = {
-	datePicker: () => ({
-		id: 'date-picker',
-		light: false,
-		onChange: console.log('onPickerChange'),
-		onClose: console.log('onClose'),
-	}),
-	datePickerInput: () => ({
-		id: 'date-picker-input-id',
-		className: 'some-class',
-		labelText: 'Date Picker label',
-		placeholder: 'mm/dd/yyyy',
-		disabled: false,
-		invalid: false,
-		invalidText: 'A valid value is required',
-		iconDescription: 'Icon description',
-		onClick: console.log('onClick'),
-		onChange: console.log('onInputChange'),
-	}),
-};
-   
-export default (
-	<div>
-		<link rel="stylesheet" type="text/css" href="https://unpkg.com/carbon-components@10.6.1/css/carbon-components.css" />
-		<DatePicker
-			{...props.datePicker()}
-			datePickerType="single"
-			dateFormat="m/d/Y"
-		>
-			<datePickerInput {...props.datePickerInput()} />
-		</DatePicker>
-	</div>
-)
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const TableDatePicker = (props) => {
+    const [date, setDate] = useState(null);
+
+    const handleChange = (date) => {
+        setDate(date)
+        props.updateDate(date)
+    }
+
+ return (
+     <div>
+         <DatePicker
+             datePickerType="single"
+             selected={date}
+             onChange={handleChange}
+             placeholder="mm/dd/yyyy"
+         />
+     </div>
+ );
+}
+
+export default TableDatePicker
