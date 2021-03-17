b61720b734307410e9c751fb2f279531c974551d
dkersten <dkersten1994@gmail.com>
2021-03-17 19:48:49 -0400

    added an activities reducer

---
+++ src/reducers/activitiesReducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    activities: [],
+    loading: false,
+    hasErrors: false,
+}
+
+export default function activitiesReducer(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
