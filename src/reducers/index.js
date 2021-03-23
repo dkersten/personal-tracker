0167ebf882d8fc238420c868991db751a3f11cc1
dkersten <dkersten1994@gmail.com>
2021-03-23 19:55:14 -0400

    added a reducer for activites month

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
 import activitiesReducer from './activitiesReducer'
+import activitiesMonthReducer from './activitiesMonthReducer'
 
 const rootReducer = combineReducers({
     activities: activitiesReducer,
+    activitiesMonth: activitiesMonthReducer,
 })
 
 export default rootReducer
