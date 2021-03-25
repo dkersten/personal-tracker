d0d9bf399173eec9e9e7f999470b78dcd71f6bde
dkersten <dkersten1994@gmail.com>
2021-03-24 20:08:24 -0400

    activities month actions to activities month reducer

--- src/reducers/activitiesMonthReducer.js
+++ src/reducers/activitiesMonthReducer.js
@@ -1,3 +1,5 @@
+import * as actions from '../actions/activityMonthActions'
+
 export const initialState = {
     activities: [],
     loading: false,
@@ -6,6 +8,12 @@ export const initialState = {
 
 export default function activitiesMonthReducer(state = initialState, action) {
     switch (action.type) {
+        case actions.GET_ACTIVITIES_MONTH:
+            return {...state, loading: true}
+        case actions.GET_ACTIVITIES_MONTH_SUCCESS:
+            return {activitiesMonth: action.payload, loading: false, hasErrors: false}
+        case actions.GET_ACTIVITIES_MONTH_FAILURE:
+            return {...state, loading: false, hasErrors: true}
         default:
             return state
     }
